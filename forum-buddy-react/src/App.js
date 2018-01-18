d42ea61e67b269140d82e7ad6429a2fe22c38f8d
Kyle Asaff <kyleasaff@pixleeteam.com>
2018-01-18 13:55:27 -0800

    bleep bloop

--- forum-buddy-react/src/App.js
+++ forum-buddy-react/src/App.js
@@ -7,11 +7,60 @@ let defaultStyle = {
   color: defaultTextColor
 };
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Kyle',
+    playlists: [
+      {
+        name: 'my favories',
+        songs: [
+          {name: 'blue1', duration: 12334},
+          {name: 'blue2', duration: 12334},
+          {name: 'blu3e', duration: 12314}
+        ]
+      },
+      {
+        name: 'my jokes',
+        songs: [
+          {name: 'bluzzze1', duration: 12334},
+          {name: 'blueddsa2', duration: 12334},
+          {name: 'blu3sadae', duration: 12314}
+        ]
+      },
+      {
+        name: 'my lopes',
+        songs: [
+          {name: 'bluse1', duration: 12334},
+          {name: 'blue2', duration: 12334},
+          {name: 'blu3e', duration: 12314}
+        ]
+      }
+    ]
+  }
+};
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    },[]);
+
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+      <div className="HoursCounter">
+        <h2 style={defaultStyle}>{totalDuration} seconds</h2>
+      </div>
+    );
+  }
+}
+
+class PlaylistCounter extends Component {
   render() {
     return (
-      <div className="aggregate">
-        <h2 style={defaultStyle}>Number and Text</h2>
+      <div className="PlaylistCounter">
+        <h2 style={defaultStyle}>{this.props.playlists.length} hours</h2>
       </div>
     );
   }
@@ -45,12 +94,27 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+  componentDidMount() {
+    this.setState({
+      serverData: fakeServerData
+    });
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-        <Aggregate />
-        <Aggregate />
+        {this.state.serverData.user &&
+          <div>
+            <h1 style={{ ...defaultStyle }}>
+              {this.state.serverData.user.name}
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+          </div>
+        }
         <Filter />
         <Playlist />
       </div>
